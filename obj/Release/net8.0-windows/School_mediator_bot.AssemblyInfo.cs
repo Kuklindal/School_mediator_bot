b755b098a097e23989af8e9bbfc6f4aa3e42c0b7
Daniil Kuklin <kuklidan@fit.cvut.cz>
2025-05-16 17:09:40 +0700

    publish

--- obj/Release/net8.0-windows/School_mediator_bot.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/School_mediator_bot.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("School_mediator_bot")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+272f66dc84dd4b33a04d7d2c533bda43fa59a412")]
 [assembly: System.Reflection.AssemblyProductAttribute("School_mediator_bot")]
